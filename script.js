492c42ea567205dcbcbd8514d203f0af9c6c9186
SaadKhan <khanssad378@gmail.com>
2024-03-06 22:42:42 +0530

    page 5

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
 function loadingAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1", {
@@ -41,7 +75,7 @@ function loadingAnimation() {
   tl.from("#nav", {
     opacity: 0,
   });
-  tl.from("#hero1 h1,#hero2 h1,#hero3 h2 ,#hero4 h1", {
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
     y: 140,
     stagger: 0.2,
   });
@@ -65,6 +99,7 @@ function cursorAnimation(){
   Shery.makeMagnet("#nav-part2 h4" );
 }
 
+locomotiveAnimation();
 loadingAnimation();
 cursorAnimation();
 
